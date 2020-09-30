4758f32de8a9b71307be1d417c3bf3c7f1a57508
RoundBearChoi <roundbeargames@gmail.com>
2020-09-30 17:46:03 +0900

    choi

---
+++ Lightbulb_binary_example/Assets/binary to int/BinaryToInt.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LightBulbBinary
+{
+    public class BinaryToInt : MonoBehaviour
+    {
+        char[] arrBinaryChar = new char[32];
+
+        [Header("Results")]
+        public string binaryOutput;
+        public int intOutput;
+        public string intStringOutput;
+
+        [Space(10)]
+
+        public List<LightBulb> ListLightBulbs_B0 = new List<LightBulb>();
+        public List<LightBulb> ListLightBulbs_B1 = new List<LightBulb>();
+        public List<LightBulb> ListLightBulbs_B2 = new List<LightBulb>();
+        public List<LightBulb> ListLightBulbs_B3 = new List<LightBulb>();
+
+        [Space(10)]
+        public Text result;
+
+        void GetBinary(int index, List<LightBulb> lightBulbs)
+        {
+            for (int i = 0; i < lightBulbs.Count; i++)
+            {
+                int b = i + (index * 8);
+
+                if (lightBulbs[i].On)
+                {
+                    arrBinaryChar[b] = '1';
+                }
+                else
+                {
+                    arrBinaryChar[b] = '0';
+                }
+            }
+        }
+
+        public void GetBinarySequence()
+        {
+            GetBinary(0, ListLightBulbs_B0);
+            GetBinary(1, ListLightBulbs_B1);
+            GetBinary(2, ListLightBulbs_B2);
+            GetBinary(3, ListLightBulbs_B3);
+
+            binaryOutput = new string(arrBinaryChar);
+            intOutput = System.Convert.ToInt32(binaryOutput, 2);
+            intStringOutput = intOutput.ToString("n0");
+
+            result.text = intStringOutput;
+        }
+    }
+}
---
+++ Lightbulb_binary_example/Assets/lightbulb/LightBulb.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LightBulbBinary
+{
+    public class LightBulb : MonoBehaviour
+    {
+        public bool On;
+        public SpriteRenderer renderer_0;
+        public SpriteRenderer renderer_1;
+        SpriteRenderer renderer_bulb;
+        BinaryToInt binaryToInt;
+
+        private void Start()
+        {
+            renderer_bulb = this.gameObject.GetComponent<SpriteRenderer>();
+            binaryToInt = FindObjectOfType<BinaryToInt>();
+        }
+
+        private void Update()
+        {
+            if (On)
+            {
+                if (!renderer_1.enabled)
+                {
+                    renderer_1.enabled = true;
+                }
+
+                if (renderer_0.enabled)
+                {
+                    renderer_0.enabled = false;
+                }
+
+                if (renderer_bulb.color != Color.yellow)
+                {
+                    renderer_bulb.color = Color.yellow;
+                }
+            }
+            else
+            {
+                if (renderer_1.enabled)
+                {
+                    renderer_1.enabled = false;
+                }
+
+                if (!renderer_0.enabled)
+                {
+                    renderer_0.enabled = true;
+                }
+
+                if (renderer_bulb.color != Color.white)
+                {
+                    renderer_bulb.color = Color.white;
+                }
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            if (On)
+            {
+                On = false;
+            }
+            else
+            {
+                On = true;
+            }
+
+            binaryToInt.GetBinarySequence();
+        }
+    }
+}
