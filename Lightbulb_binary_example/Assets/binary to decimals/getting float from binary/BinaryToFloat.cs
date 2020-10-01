08381735dba954d0db6eeb784dbe3ab50c712cc6
RoundBearChoi <roundbeargames@gmail.com>
2020-10-01 18:36:23 +0900

    exponent corrected (not int)

--- Lightbulb_binary_example/Assets/binary to decimals/getting float from binary/BinaryToFloat.cs
+++ Lightbulb_binary_example/Assets/binary to decimals/getting float from binary/BinaryToFloat.cs
@@ -6,11 +6,11 @@ namespace LightBulbBinary
 {
     public static class BinaryToFloat
     {
-        static int two_twentythird = 8388608;
+        static int power_two_23 = 8388608;
 
         public static decimal GetFloatingPoint(string binaryString)
         {
-            int E = GetExponent(binaryString);
+            decimal E = GetExponent(binaryString);
             Debug.Log("E: " + E);
 
             decimal M = GetMantissa(binaryString);
@@ -19,23 +19,38 @@ namespace LightBulbBinary
             return CalcFloatingPoint(binaryString, M, E);
         }
 
-        static int GetExponent(string binaryString)
-        {
-            string exponentString = binaryString.Substring(1, 8);
-            int intResult = System.Convert.ToInt32(exponentString, 2);
-
-            return (int)Mathf.Pow(2, intResult - 127);
-        }
-
         static decimal GetMantissa(string binaryString)
         {
-            string mantissaString = binaryString.Substring(9);
-            int intResult = System.Convert.ToInt32(mantissaString, 2);
+            string twentythree_bits = binaryString.Substring(9); // 111 1111 1111 1111 1111 1111
+            int seven_digit_int = System.Convert.ToInt32(twentythree_bits, 2); // max = 8,388,607 (what you can express with 23 bits)
 
-            return 1 + (decimal)intResult / two_twentythird;
+            return 1 + (decimal)seven_digit_int / power_two_23; // "power_two_23" = 2^23 = 8,388,608
+
+            // highest result is approx 1.9999999
+            // lowest result is approx 1.0000001
         }
 
-        static decimal CalcFloatingPoint(string binaryString, decimal mantissa, int exponent)
+        static decimal GetExponent(string binaryString)
+        {
+            string exponent_part = binaryString.Substring(1, 8); // 1111 1111
+            int intResult = System.Convert.ToInt32(exponent_part, 2); // max = 128 (what you can express with 8 bits)
+
+            return (int)Mathf.Pow(2, intResult - 127);
+
+            // 2^1 = 2
+            // 2^0 = 1
+            // 2^-1 = 0.5
+            // 2^-2 = 0.25
+            // 2^-3 = 0.125
+            // 2^-4 = 0.0625
+            // 2^-5 = 0.03125
+            // 2^-6 = 0.015625
+            // 2^-7 = 0.0078125
+            // ...
+            // 2^-127 = close to 0
+        }
+
+        static decimal CalcFloatingPoint(string binaryString, decimal mantissa, decimal exponent)
         {
             int sign = GetSign(binaryString);
             return sign * mantissa * exponent;
